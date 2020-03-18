734e87cc59b08c99cb98bb8de378d6c3ab81cacc
Dan Stoian <danmihaistoian@gmail.com>
2020-03-18 02:26:42 +0200

    functional site

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 
-ng build --prod --output-path docs
+ng build --prod --output-path docs --base-href church-notice-board
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { AboutComponent } from './about/about.component';
+import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+
+
+const appRoutes: Routes = [
+  {path: 'posts', component: PostComponent},
+  {path: 'about', component: AboutComponent },
+  {path: 'contact', component: ContactComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    AboutComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AppRoutingModule
   ],
