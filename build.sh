45e111a87dc4f4b26982f7a9099c822f8ae59201
Dan Stoian <danmihaistoian@gmail.com>
2020-04-04 13:03:12 +0300

    Fix broke page

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # --base-href / if this is not present username.github.io/church-notice-board will redirect to username.github.io/church-notice-board/church-notice-board
 # and that is akword... 
-ng build --prod --output-path docs --base-href /
+ng build --prod --output-path docs --base-href /church-notice-board/
 
 
 # Angular build documenation, needed for fixing broken url 
