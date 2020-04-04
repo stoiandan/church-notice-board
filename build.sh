63dcbd0aa8ea9d26bcd96bac8bd4d932f3d52fdf
Dan Stoian <danmihaistoian@gmail.com>
2020-04-04 13:10:55 +0300

    comments on build

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-# --base-href / if this is not present username.github.io/church-notice-board will redirect to username.github.io/church-notice-board/church-notice-board
+# --base-href /church-notice-board/ AND NOT church-notice-board/ (notice the missing slash at the begining of the URL) if this is not present username.github.io/church-notice-board will redirect to username.github.io/church-notice-board/church-notice-board
 # and that is akword... 
 ng build --prod --output-path docs --base-href /church-notice-board/
 
